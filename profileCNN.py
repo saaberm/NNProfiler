8607e1a3bbe573753df67bbddbf260d87146267b
Saber M <63671541+graimatterlabs@users.noreply.github.com>
2020-05-19 22:18:30 -0700

    added first version of NNProfiler + a test
    This is first version; profiling operations and parameters for 2d and 3D CNNs;

---
+++ profileCNN.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+
+def profile_conv3d(mod, inp, outp): # model, input, output
+    Bias_add = 1 if mod.bias is not None else 0
+    ChIn = mod.in_channels // mod.groups
+    Ops = ChIn * mod.kernel_size[0] * mod.kernel_size[1] * mod.kernel_size[2] + (ChIn * mod.kernel_size[0] * mod.kernel_size[1] * mod.kernel_size[2] -1) + Bias_add
+    TotalOps = outp.numel() * Ops
+    #print("Conv3d ops {:.2e}".format(TotalOps))
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_conv2d(mod, inp, outp):
+    Bias_add = 1 if mod.bias is not None else 0
+    ChIn = mod.in_channels // mod.groups
+    Ops = ChIn * mod.kernel_size[0] * mod.kernel_size[1] + (ChIn * mod.kernel_size[0] * mod.kernel_size[1] -1) + Bias_add # number of operations: multi + addition + biasses_add
+    TotalOps = outp.numel() * Ops
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_linear(mod, inp, outp):
+    TotalOps = (mod.in_features + (mod.in_features -1)) * outp.numel() # (mult+addition)*num_out
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_BatchNorm3d(mod, inp, outp):
+    TotalOps = inp[0].numel() + inp[0].numel() # there is one substraction and one division per input element 
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_BatchNorm2d(m, inp, outp):    
+    TotalOps = inp[0].numel() + inp[0].numel() # there is one substraction and one division per input element 
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_relu(mod, inp, outp):
+    TotalOps = inp[0].numel()
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_softmax(mod, inp, outp):
+    BatchSize, NumFeatures = inp[0].size()
+    TotalOps = BatchSize * (inp[0].size() + inp[0].size() + inp[0].size()) #there is one exp, one addition and one division per class(batch element) 
+    mod.TotalOps += torch.Tensor([int(Ops)])
+
+def profile_maxpool(mod, inp, outp):
+    Ops = (torch.prod(torch.Tensor([mod.kernel_size])) - 1) * outp.numel()
+    mod.TotalOps += torch.Tensor([int(Ops)])
+
+def profile_avgpool(mod, inp, outp):
+    Ops = (torch.prod(torch.Tensor([mod.kernel_size]) - 1) + 1) * outp.numel() #addition + div * output_elements 
+    mod.TotalOps += torch.Tensor([int(Ops)])
+
+def NN_Profiler(model, inp_size):
+
+    model.eval()
+    print ("model loaded")
+
+    def Lookup(mod):
+        if len(list(mod.children())) > 0: return
+        mod.register_buffer('TotalOps', torch.zeros(1))
+        mod.register_buffer('TotalParams', torch.zeros(1))
+
+        for param in mod.parameters():
+            mod.TotalParams += torch.Tensor([param.numel()])
+
+        if isinstance(mod, nn.Conv3d):
+            mod.register_forward_hook(profile_conv3d)
+
+        elif isinstance(mod, nn.Conv2d):
+            mod.register_forward_hook(profile_conv2d)
+
+        elif isinstance(mod, nn.Linear):
+            mod.register_forward_hook(profile_linear)
+
+        elif isinstance(mod, nn.ReLU):
+            mod.register_forward_hook(profile_relu)
+
+        elif isinstance(mod, nn.BatchNorm3d):
+            mod.register_forward_hook(profile_BatchNorm3d)
+	
+        elif isinstance(mod, nn.BatchNorm2d):
+            mod.register_forward_hook(profile_BatchNorm2d)
+
+        elif isinstance(mod, (nn.MaxPool3d, nn.MaxPool2d)):
+            mod.register_forward_hook(profile_maxpool)
+
+        elif isinstance(mod, (nn.AvgPool3d, nn.AvgPool2d)):
+            mod.register_forward_hook(profile_avgpool)
+
+        else:
+            print("Not yet implemented for the layer ", mod)
+
+    model.apply(Lookup)
+    model(torch.zeros(inp_size))
+
+    Ops = 0
+    Params = 0
+    for modu in model.modules():
+        if len(list(modu.children())) > 0: continue
+        Ops += modu.TotalOps
+        Params += modu.TotalParams
+        #print(modu)
+
+    return Ops, Params
---
+++ test.py
@@ -0,0 +1,63 @@
+#import profiler and model 
+from profileCNN import NN_Profiler
+from functions import *
+
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch.utils.data as data
+import torchvision
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+from functions import *
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.metrics import accuracy_score
+import pickle
+
+
+#check for alexnet
+model = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=True)
+inp_size = (1, 3, 224,224)
+
+#uncomment for testing 3D CNN 
+#this model is imported from https://github.com/HHTseng/video-classification
+
+"""
+# 3D CNN parameters
+fc_hidden1, fc_hidden2 = 256, 256
+dropout = 0.0        # dropout probability
+
+# training parameters
+k = 101            # number of target category
+epochs = 15
+batch_size = 30
+learning_rate = 1e-4
+log_interval = 10
+img_x, img_y = 256, 342  # resize video 2d frame size
+
+# Select which frame to begin & end in videos
+begin_frame, end_frame, skip_frame = 1, 29, 1
+selected_frames = np.arange(begin_frame, end_frame, skip_frame).tolist()
+# Detect devices
+use_cuda = torch.cuda.is_available()                   # check if GPU exists
+device = torch.device("cuda" if use_cuda else "cpu")   # use CPU or GPU
+
+# load UCF101 actions names
+params = {'batch_size': batch_size, 'shuffle': True, 'num_workers': 4, 'pin_memory': True} if use_cuda else {}
+
+
+
+model = CNN3D(t_dim=len(selected_frames), img_x=img_x, img_y=img_y,
+              drop_p=dropout, fc_hidden1=fc_hidden1,  fc_hidden2=fc_hidden2, num_classes=k).to(device)
+inp_size=(1, 1, 28, 256, 342)
+"""
+
+#profiling example net
+ops, paramz = NN_Profiler(model, inp_size)
+print ("Total OPS", int(ops)/1e9, "GOPs")# {:.2e}".format(int(ops)))
+print ("Total params", int(paramz)/1e6, "Meg")# {:.2e}".format(int(params)))
