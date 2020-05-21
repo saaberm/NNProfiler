eea7b340c21ac9dd261cb8495e945418d846c34a
graimatterlabs <smoradi@graimatterlabs.ai>
2020-05-21 13:09:55 -0700

    added neurons and syn numbers calculation

--- profileCNN.py
+++ profileCNN.py
@@ -6,19 +6,36 @@ def profile_conv3d(mod, inp, outp): # model, input, output
     ChIn = mod.in_channels // mod.groups
     Ops = ChIn * mod.kernel_size[0] * mod.kernel_size[1] * mod.kernel_size[2] + (ChIn * mod.kernel_size[0] * mod.kernel_size[1] * mod.kernel_size[2] -1) + Bias_add
     TotalOps = outp.numel() * Ops
+    TotalSynps = (mod.out_channels // mod.groups) * ChIn * mod.kernel_size[0] * mod.kernel_size[1] * mod.kernel_size[2]
     #print("Conv3d ops {:.2e}".format(TotalOps))
+    TotalNeurons = outp.numel() 
     mod.TotalOps += torch.Tensor([int(TotalOps)])
+    mod.TotalSynps += torch.Tensor([int(TotalSynps)])
+    mod.TotalNeurons += torch.Tensor([int(TotalNeurons)])
 
 def profile_conv2d(mod, inp, outp):
     Bias_add = 1 if mod.bias is not None else 0
     ChIn = mod.in_channels // mod.groups
     Ops = ChIn * mod.kernel_size[0] * mod.kernel_size[1] + (ChIn * mod.kernel_size[0] * mod.kernel_size[1] -1) + Bias_add # number of operations: multi + addition + biasses_add
     TotalOps = outp.numel() * Ops
+    TotalSynps = (mod.out_channels // mod.groups) * ChIn * mod.kernel_size[0] * mod.kernel_size[1]
+    TotalNeurons = outp.numel()
+    #print ("out size ", outp.size())
+    #print("Conv2d neurons (K) ", TotalNeurons/1e3)
     mod.TotalOps += torch.Tensor([int(TotalOps)])
+    mod.TotalSynps += torch.Tensor([int(TotalSynps)])
+    mod.TotalNeurons += torch.Tensor([int(TotalNeurons)])
+
 
 def profile_linear(mod, inp, outp):
     TotalOps = (mod.in_features + (mod.in_features -1)) * outp.numel() # (mult+addition)*num_out
     mod.TotalOps += torch.Tensor([int(TotalOps)])
+    TotalSynps = outp.numel() * mod.in_features 
+    TotalNeurons = outp.numel()
+    mod.TotalSynps += torch.Tensor([int(TotalSynps)])
+    mod.TotalNeurons += torch.Tensor([int(TotalNeurons)])
+    #print("FC neurons (K) ", TotalNeurons/1e3)
+
 
 def profile_BatchNorm3d(mod, inp, outp):
     TotalOps = inp[0].numel() + inp[0].numel() # there is one substraction and one division per input element 
@@ -30,6 +47,13 @@ def profile_BatchNorm2d(m, inp, outp):
 
 def profile_relu(mod, inp, outp):
     TotalOps = inp[0].numel()
+    mod.TotalOps += torch.Tensor([int(TotalOps)])
+
+def profile_sigmoid(mod, inp, outp):
+    ExpOps = inp[0].numel()
+    AddOps = inp[0].numel()
+    DivOps = inp[0].numel()
+    Ops = ExpOps + AddOps + DivOps  
     mod.TotalOps += torch.Tensor([int(TotalOps)])
 
 def profile_softmax(mod, inp, outp):
@@ -54,6 +78,8 @@ def NN_Profiler(model, inp_size):
         if len(list(mod.children())) > 0: return
         mod.register_buffer('TotalOps', torch.zeros(1))
         mod.register_buffer('TotalParams', torch.zeros(1))
+        mod.register_buffer('TotalNeurons', torch.zeros(1))
+        mod.register_buffer('TotalSynps', torch.zeros(1))
 
         for param in mod.parameters():
             mod.TotalParams += torch.Tensor([param.numel()])
@@ -66,6 +92,9 @@ def NN_Profiler(model, inp_size):
 
         elif isinstance(mod, nn.Linear):
             mod.register_forward_hook(profile_linear)
+
+        elif isinstance(mod, nn.Sigmoid):
+            mod.register_forward_hook(profile_sigmoid)
 
         elif isinstance(mod, nn.ReLU):
             mod.register_forward_hook(profile_relu)
@@ -90,10 +119,14 @@ def NN_Profiler(model, inp_size):
 
     Ops = 0
     Params = 0
+    Neurons = 0 
+    Synps = 0
     for modu in model.modules():
         if len(list(modu.children())) > 0: continue
         Ops += modu.TotalOps
         Params += modu.TotalParams
+        Neurons += modu.TotalNeurons
+        Synps += modu.TotalSynps
         #print(modu)
 
-    return Ops, Params
+    return Ops, Params, Neurons, Synps
